9f9475f04b73896cd7efea5d28cf7eee0a27b791
Waq jel <omnipotentdeus1@gmail.com>
2021-09-07 21:22:07 +0200

    initial commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+#(Flask instances
+app = Flask(__name__)
+
+# route decorator
+@app.route('/')
+
+def index():
+    first_name = "John"
+    return render_template("index.html", first_name=first_name)
+
+
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", name=name)
+
+
+#Invalid page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500  
