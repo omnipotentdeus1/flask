676694cc4e67b40f382ac813cff76b2fe92a0e27
Waq jel <omnipotentdeus1@gmail.com>
2021-09-07 21:22:52 +0200

    initial commit

--- hello.py
+++ hello.py
@@ -16,7 +16,6 @@ def index():
 def user(name):
     return render_template("user.html", name=name)
 
-
 #Invalid page
 @app.errorhandler(404)
 def page_not_found(e):
@@ -25,4 +24,4 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template("500.html"), 500  
+    return "render_template("500.html"), 500  
