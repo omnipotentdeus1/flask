4f9d804c97ed6d09624652a262e5d13e74f6510b
Waqar Jee <omnipotentdeus1@gmail.com>
2021-09-07 22:10:16 +0200

    tweaked 500.html

--- hello.py
+++ hello.py
@@ -16,12 +16,12 @@ def index():
 def user(name):
     return render_template("user.html", name=name)
 
-#Invalid page
+#Invalid page error
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
-
+#Internal server error
 @app.errorhandler(500)
 def page_not_found(e):
     return "render_template("500.html"), 500  
